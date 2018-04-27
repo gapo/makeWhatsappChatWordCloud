7649274018b5e01e95e5b861fef530d0953c7a91
Gautham Ponnu <gautham.go@gmail.com>
2018-04-27 16:40:56 -0400

    Added makeWhatsappChatWordCloud.m

---
+++ makeWhatsappChatWordCloud.m
@@ -0,0 +1,68 @@
+function makeWhatsappChatWordCloud(varargin)
+
+numvarargs = length(varargin);
+    if numvarargs > 2
+        error('makeWhatsappChatWordCloud:TooManyInputs', ...
+        'requires at most 1 input');
+    elseif (numvarargs == 1)
+        fileName = varargin(1);
+    else
+        disp 'No filename has been sepcified';
+        names = dir("*.txt");
+        fileName = char({names.name});
+        disp 'Using below file instead';
+        disp(fileName);
+    end
+    
+% Read File
+chats = string(fileread(fileName));
+
+% Remove all the date+first few letters
+chat= split(chats,newline);
+
+% Go through the file to remove Names and timestamps
+for i=1:1:size(chat)
+    splitStrings = regexp(chat{i},':','split');
+    [~,sz] = size(splitStrings);
+    if sz==3
+        chat{i}=splitStrings{3};
+    elseif sz>3
+        chat{i}=char(join(splitStrings(3:end),':'));
+    elseif sz==1 || sz<3
+        continue;
+    else
+        assert(true,'woah - unexpecte case');
+    end
+end
+
+% Now remove all the common punctuations
+punctuationCharacters = ["." "?" "!" "," ";"];
+sonnets = replace(chat,punctuationCharacters," ");
+words = split(join(sonnets));
+
+% Remove all words of size less than 2
+words(strlength(words)<2) = [];
+
+% Convert to lower case
+words = lower(words);
+
+% Create a categorical for stuff 
+C = categorical(words);
+
+% Now remove Red Herrings
+% remove <Media Omitted>
+C = removecats(C,'<media');
+C = removecats(C,'omitted>');
+% remove https://www
+C = removecats(C,'https://www');
+C = removecats(C,'http://www');
+
+% remove stopWords
+stpWords = {'a', 'about', 'above', 'above', 'across', 'after', 'afterwards', 'again', 'against', 'all', 'almost', 'alone', 'along', 'already', 'also','although','always','am','among', 'amongst', 'amoungst', 'amount',  'an', 'and', 'another', 'any','anyhow','anyone','anything','anyway', 'anywhere', 'are', 'around', 'as',  'at', 'back','be','became', 'because','become','becomes', 'becoming', 'been', 'before', 'beforehand', 'behind', 'being', 'below', 'beside', 'besides', 'between', 'beyond', 'bill', 'both', 'bottom','but', 'by', 'call', 'can', 'cannot', 'cant', 'co', 'con', 'could', 'couldnt', 'cry', 'de', 'describe', 'detail', 'do', 'done', 'down', 'due', 'during', 'each', 'eg', 'eight', 'either', 'eleven','else', 'elsewhere', 'empty', 'enough', 'etc', 'even', 'ever', 'every', 'everyone', 'everything', 'everywhere', 'except', 'few', 'fifteen', 'fify', 'fill', 'find', 'fire', 'first', 'five', 'for', 'former', 'formerly', 'forty', 'found', 'four', 'from', 'front', 'full', 'further', 'get', 'give', 'go', 'had', 'has', 'hasnt', 'have', 'he', 'hence', 'her', 'here', 'hereafter', 'hereby', 'herein', 'hereupon', 'hers', 'herself', 'him', 'himself', 'his', 'how', 'however', 'hundred', 'ie', 'if', 'in', 'inc', 'indeed', 'interest', 'into', 'is', 'it', 'its', 'itself', 'keep', 'last', 'latter', 'latterly', 'least', 'less', 'ltd', 'made', 'many', 'may', 'me', 'meanwhile', 'might', 'mill', 'mine', 'more', 'moreover', 'most', 'mostly', 'move', 'much', 'must', 'my', 'myself', 'name', 'namely', 'neither', 'never', 'nevertheless', 'next', 'nine', 'no', 'nobody', 'none', 'noone', 'nor', 'not', 'nothing', 'now', 'nowhere', 'of', 'off', 'often', 'on', 'once', 'one', 'only', 'onto', 'or', 'other', 'others', 'otherwise', 'our', 'ours', 'ourselves', 'out', 'over', 'own','part', 'per', 'perhaps', 'please', 'put', 'rather', 're', 'same', 'see', 'seem', 'seemed', 'seeming', 'seems', 'serious', 'several', 'she', 'should', 'show', 'side', 'since', 'sincere', 'six', 'sixty', 'so', 'some', 'somehow', 'someone', 'something', 'sometime', 'sometimes', 'somewhere', 'still', 'such', 'system', 'take', 'ten', 'than', 'that', 'the', 'their', 'them', 'themselves', 'then', 'thence', 'there', 'thereafter', 'thereby', 'therefore', 'therein', 'thereupon', 'these', 'they', 'thickv', 'thin', 'third', 'this', 'those', 'though', 'three', 'through', 'throughout', 'thru', 'thus', 'to', 'together', 'too', 'top', 'toward', 'towards', 'twelve', 'twenty', 'two', 'un', 'under', 'until', 'up', 'upon', 'us', 'very', 'via', 'was', 'we', 'well', 'were', 'what', 'whatever', 'when', 'whence', 'whenever', 'where', 'whereafter', 'whereas', 'whereby', 'wherein', 'whereupon', 'wherever', 'whether', 'which', 'while', 'whither', 'who', 'whoever', 'whole', 'whom', 'whose', 'why', 'will', 'with', 'within', 'without', 'would', 'yet', 'you', 'your', 'yours', 'yourself', 'yourselves', 'the'};
+C = removecats(C,stpWords);
+
+% Plot figure
+figure
+wordcloud(C);
+
+end
